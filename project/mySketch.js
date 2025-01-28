485469c88c7dd665d1560a61089fde90f1073790
DanielWydler <26daniel.wydler@lwhs.org>
2025-01-28 23:22:39 +0000

    Heart Card

--- project/mySketch.js
+++ project/mySketch.js
@@ -3,6 +3,14 @@ function setup() {
 }
 
 function draw() {
-  background(200);
-  
+  background("blue");
+  fill("red")
+  text("Hello I love you pooki bear", 100, 50);
+  textSize (20)
+  fill("pink")
+  circle(200, 200, 50)
+  fill("Pink")
+  circle(150, 200, 50)
+  fill("pink")
+  triangle(100, 500, 20, 400, 300, 100)
 }
